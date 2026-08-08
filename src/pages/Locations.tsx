dbbd28aab60c4ac7e8763b530e1e0ef33de4e7f0
titomundo <tito.alberto.mundo@gmail.com>
2026-08-07 23:53:43 -0600

    add simple location pages

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,3 +1,61 @@
+import { TitleBar } from "../components/TitleBar";
+import { useAuth } from "../utils/AuthContext";
+import { useEffect, useState } from "react";
+
+interface Location {
+  id: string;
+  name: string;
+  description: string;
+  capacity: number;
+  business_id: string;
+}
+
 export function Locations() {
-  return <h1>Locations</h1>;
+  const [locations, setLocations] = useState<Array<Location>>([]);
+  const isAuthenticated = useAuth();
+  const token = isAuthenticated ? localStorage.getItem("token") : null;
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("http://127.0.0.1:5000/api/v1/locations/", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.msg) {
+            console.log("Invalid credentials");
+            return;
+          }
+
+          setLocations(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    fetchData();
+  }, [token]);
+
+  return (
+    <div>
+      <TitleBar title="Locations" />
+      <div className="flex gap-3 w-full">
+        {locations.map((e) => (
+          <div
+            className="text-sm bg-gray-100 border border-gray-200 px-3 py-3 rounded-md text-left"
+            key={e.id}
+          >
+            <h3 className="font-semibold">{e.name}</h3>
+            <p>{`Description: ${e.description}`}</p>
+            <p>{`Capacity: ${e.capacity}`}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
