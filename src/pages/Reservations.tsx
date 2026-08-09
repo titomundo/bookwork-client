3f1fd39edeff6c7e76d495c934395aba577b4aaa
titomundo <tito.alberto.mundo@gmail.com>
2026-08-09 01:38:52 -0600

    reservation filtering

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../utils/AuthContext";
 import { TitleBar } from "../components/TitleBar";
+import { format } from "date-fns";
 
 interface Reservation {
   id: string;
@@ -14,9 +15,30 @@ interface Reservation {
 }
 
 export function Reservations() {
-  const [reservations, setReservations] = useState<Array<Reservation>>([]);
   const isAuthenticated = useAuth();
   const token = isAuthenticated ? localStorage.getItem("token") : null;
+  const [reservations, setReservations] = useState<Array<Reservation>>([]);
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  function filterDate(date: string) {
+    const current = new Date(date);
+
+    if (!startDate || !endDate) {
+      return true;
+    }
+
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    return start <= current && end >= current;
+  }
+
+  function clearDate() {
+    document.getElementById("startDate").value = "";
+    document.getElementById("endDate").value = "";
+    setStartDate(null);
+    setEndDate(null);
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -47,19 +69,71 @@ export function Reservations() {
   return (
     <div>
       <TitleBar title="Reservations" />
-      <div className="flex gap-3 w-full">
-        {reservations.map((e) => (
-          <div
-            className="text-sm bg-gray-100 border border-gray-200 px-3 py-3 rounded-md text-left"
-            key={e.id}
-          >
-            <h3 className="font-semibold">{e.date}</h3>
-            <p>{`Client: ${e.client_name}`}</p>
-            <p>{`Reason: ${e.reason}`}</p>
-            <p>{`Slot: ${e.slot}`}</p>
-            <p>{`Status: ${e.status}`}</p>
-          </div>
-        ))}
+
+      <div className="flex gap-2 text-left my-6 text-xs">
+        <input
+          id="startDate"
+          name="startDate"
+          type="date"
+          onChange={(e) => setStartDate(new Date(e.target.value))}
+          className="border border-gray-200 rounded-md py-1 px-2"
+        />
+        <span className="py-1 font-semibold">to</span>
+        <input
+          id="endDate"
+          name="endDate"
+          type="date"
+          onChange={(e) => setEndDate(new Date(e.target.value))}
+          className="border border-gray-200 rounded-md py-1 px-2"
+        />
+        <button
+          className="bg-sky-100 border border-gray-200 rounded-md py-1 px-2"
+          onClick={clearDate}
+        >
+          Clear filter
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-2 w-full">
+        {reservations
+          .filter((e) => filterDate(e.date))
+          .map((e) => {
+            const date = new Date(e.date);
+
+            return (
+              <div
+                className="text-sm border border-gray-200 px-3 py-3 rounded-md text-right flex gap-1"
+                key={e.id}
+              >
+                <div>
+                  <h3 className="font-semibold">
+                    {format(date, "dd-MM-yyyy")}
+                  </h3>
+                  <h3 className="font-semibold">
+                    {format(date, "hh:mm aaaa")}
+                  </h3>
+                </div>
+                <dl>
+                  <div className="flex gap-1">
+                    <dt className="text-right font-semibold">Client:</dt>
+                    <dd>{e.client_name}</dd>
+                  </div>
+                  <div className="flex gap-2">
+                    <dt className="text-right font-semibold">Reason:</dt>
+                    <dd>{e.reason}</dd>
+                  </div>
+                  <div className="flex gap-2">
+                    <dt className="text-right font-semibold">Status:</dt>
+                    <dd>{e.status}</dd>
+                  </div>
+                  <div className="flex gap-2">
+                    <dt className="text-right font-semibold">Slot:</dt>
+                    <dd>{e.slot}</dd>
+                  </div>
+                </dl>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
