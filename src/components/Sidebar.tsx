3ab7d805090554808b669ed60d74c77dbcfa411b
titomundo <tito.alberto.mundo@gmail.com>
2026-08-09 13:14:49 -0600

    add colors to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,30 @@
 import { NavLink } from "react-router-dom";
 import { Logout } from "./Logout";
+import {
+  CalendarDaysIcon,
+  BuildingOfficeIcon,
+  BriefcaseIcon,
+  UserIcon,
+  UsersIcon,
+} from "@heroicons/react/16/solid";
+import { Users } from "../pages/Users";
 
 export function Sidebar() {
   const active =
-    "py-1 px-3 ml-1 bg-emerald-700/80 text-gray-100 font-semibold rounded-md";
+    "py-0.5 px-1.5 bg-emerald-700/20 hover:bg-emerald-700/30 font-medium rounded-sm w-full block text-emerald-800 flex items-center";
 
   return (
-    <div className="p-6 text-left bg-gray-100 h-full border-r border-r-gray-200">
-      <h1 className="font-semibold text-lg text-center">Bookworm</h1>
-      <ul className="flex flex-col gap-3 my-8 text-sm font-semibold">
+    <div className="px-4 w-48 text-left bg-gray-50 h-full border-r border-r-gray-200 text-gray-600">
+      <h1 className="px-2 py-4 font-semibold text-lg text-gray-700">
+        Bookworm
+      </h1>
+      <ul className="flex flex-col gap-1 my-2 text-sm w-full font-medium">
         <li>
           <NavLink
             to="/dashboard"
-            className={({ isActive }) => (isActive ? active : "")}
+            className={({ isActive }) =>
+              isActive ? active : "px-2 w-full block"
+            }
           >
             Dashboard
           </NavLink>
@@ -20,41 +32,56 @@ export function Sidebar() {
         <li>
           <NavLink
             to="/businesses"
-            className={({ isActive }) => (isActive ? active : "")}
+            className={({ isActive }) =>
+              isActive ? active : "px-2 flex items-center"
+            }
           >
-            Businesses
+            <BriefcaseIcon className="size-6 pr-2" />
+            <span>Businesses</span>
           </NavLink>
         </li>
         <li>
           <NavLink
             to="/locations"
-            className={({ isActive }) => (isActive ? active : "")}
+            className={({ isActive }) =>
+              isActive ? active : "px-2 flex items-center"
+            }
           >
-            Locations
+            <BuildingOfficeIcon className="size-6 pr-2" />
+            <span>Locations</span>
           </NavLink>
         </li>
         <li>
           <NavLink
             to="/reservations"
-            className={({ isActive }) => (isActive ? active : "")}
+            className={({ isActive }) =>
+              isActive ? active : "px-2 flex items-center"
+            }
           >
-            Reservations
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/profile"
-            className={({ isActive }) => (isActive ? active : "")}
-          >
-            Profile
+            <CalendarDaysIcon className="size-6 pr-2" />
+            <span>Reservations</span>
           </NavLink>
         </li>
         <li>
           <NavLink
             to="/users"
-            className={({ isActive }) => (isActive ? active : "")}
+            className={({ isActive }) =>
+              isActive ? active : "px-2 flex items-center"
+            }
           >
-            Users
+            <UsersIcon className="size-6 pr-2" />
+            <span>Users</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) =>
+              isActive ? active : "px-2 flex items-center"
+            }
+          >
+            <UserIcon className="size-6 pr-2" />
+            <span>Profile</span>
           </NavLink>
         </li>
       </ul>
