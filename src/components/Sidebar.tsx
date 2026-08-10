07b658a901927ff10e248ecf5b2b98a88ddc145d
titomundo <tito.alberto.mundo@gmail.com>
2026-08-10 02:25:51 -0600

    cleaner syntax

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,8 +9,12 @@ import {
 } from "@heroicons/react/16/solid";
 
 export function Sidebar() {
-  const active =
-    "py-0.5 px-1.5 bg-emerald-700/20 hover:bg-emerald-700/30 font-medium rounded-sm w-full block text-emerald-800 flex items-center";
+  const active = "bg-emerald-700/20 hover:bg-emerald-700/30 text-emerald-800";
+  const base = " py-0.5 px-1.5 flex items-center rounded-sm";
+
+  function getClass(isActive: boolean) {
+    return isActive ? active + base : base;
+  }
 
   return (
     <div className="px-4 w-48 text-left bg-gray-50 h-full border-r border-r-gray-200 text-gray-600">
@@ -18,22 +22,20 @@ export function Sidebar() {
         Bookworm
       </h1>
       <ul className="flex flex-col gap-1 my-2 text-sm w-full font-medium">
+        {/*
         <li>
           <NavLink
             to="/dashboard"
-            className={({ isActive }) =>
-              isActive ? active : "px-2 w-full block"
-            }
+            className={({ isActive }) => getClass(isActive)}
           >
             Dashboard
           </NavLink>
         </li>
+        */}
         <li>
           <NavLink
             to="/businesses"
-            className={({ isActive }) =>
-              isActive ? active : "px-2 flex items-center"
-            }
+            className={({ isActive }) => getClass(isActive)}
           >
             <BriefcaseIcon className="size-6 pr-2" />
             <span>Businesses</span>
@@ -42,9 +44,7 @@ export function Sidebar() {
         <li>
           <NavLink
             to="/locations"
-            className={({ isActive }) =>
-              isActive ? active : "px-2 flex items-center"
-            }
+            className={({ isActive }) => getClass(isActive)}
           >
             <BuildingOfficeIcon className="size-6 pr-2" />
             <span>Locations</span>
@@ -53,31 +53,24 @@ export function Sidebar() {
         <li>
           <NavLink
             to="/reservations"
-            className={({ isActive }) =>
-              isActive ? active : "px-2 flex items-center"
-            }
+            className={({ isActive }) => getClass(isActive)}
           >
             <CalendarDaysIcon className="size-6 pr-2" />
             <span>Reservations</span>
           </NavLink>
         </li>
+        {/*
         <li>
-          <NavLink
-            to="/users"
-            className={({ isActive }) =>
-              isActive ? active : "px-2 flex items-center"
-            }
-          >
+          <NavLink to="/users" className={({ isActive }) => getClass(isActive)}>
             <UsersIcon className="size-6 pr-2" />
             <span>Users</span>
           </NavLink>
         </li>
+        */}
         <li>
           <NavLink
             to="/profile"
-            className={({ isActive }) =>
-              isActive ? active : "px-2 flex items-center"
-            }
+            className={({ isActive }) => getClass(isActive)}
           >
             <UserIcon className="size-6 pr-2" />
             <span>Profile</span>
